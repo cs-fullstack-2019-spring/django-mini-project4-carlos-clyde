da3530e1688ba13f322a7acc078f23baafc941ec
clyde5649 <clyde.bledsoe@yahoo.com>
2019-03-05 11:07:40 -0600

    ...

--- gameProject/gameApp/views.py
+++ gameProject/gameApp/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import UserModel,GameModel
+from django.contrib.auth.models import User
 
 
 
 def index(request):
     return HttpResponse("Hello, carlos")
+    return render(request,'gameApp/index')
 
 
 def creatuser(request):
     return HttpResponse('hello, clyde')
+
+
+
