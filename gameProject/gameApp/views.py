1f4285929031691ca50d15d8893d7c1dfa067df2
clyde5649 <clyde.bledsoe@yahoo.com>
2019-03-05 11:26:44 -0600

    clyde

--- gameProject/gameApp/views.py
+++ gameProject/gameApp/views.py
@@ -1,22 +1,20 @@
-<<<<<<< HEAD
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import UserModel,GameModel
 from django.contrib.auth.models import User
 
-=======
 from django.shortcuts import render, redirect, get_object_or_404
->>>>>>> 8c76269801dffbb2bc1b665965c56ab44c89cc7f
+
 
 # This view handles the landing page
 from .forms import NewUserForm
 from .models import UserModel, GameModel
 
 def index(request):
-<<<<<<< HEAD
+
     return HttpResponse("Hello, carlos")
     return render(request,'gameApp/index')
-=======
+
     return render(request, 'gameApp/index.html',)
 
 def createuser(request):
@@ -41,15 +39,8 @@ def deleteuser(request, id):
     if request.method == 'POST':
         user.delete()
         return redirect('index')
->>>>>>> 8c76269801dffbb2bc1b665965c56ab44c89cc7f
 
     return render(request, 'gameApp/index.html', {'selecteduser':user})
 
-<<<<<<< HEAD
 def creatuser(request):
     return HttpResponse('hello, clyde')
-
-
-
-=======
->>>>>>> 8c76269801dffbb2bc1b665965c56ab44c89cc7f
