b45255f5dcfb120f94f9d29f86c5b7c8135615e8
clyde5649 <clyde.bledsoe@yahoo.com>
2019-03-05 09:52:13 -0600

    second push

--- gameProject/gameApp/models.py
+++ gameProject/gameApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class GameModels(models.Model):
+    username = models.CharField(max_length=20)
+    password1 = models.CharField(max_length=10)
+    password2 = models.CharField(max_length=10)
+    dateaccountcreate = models.DateField()
+    rank = models.IntegerField(max_length=2,)
+    course_teacher = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True)
